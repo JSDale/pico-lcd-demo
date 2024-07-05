c45b39d35b9eb3b8b3d3af1a519f687eef4dcd40
JSDALE <jdale994@googlemail.com>
2024-07-05 23:36:14 +0100

    slowly creating an env

---
+++ gfx_pack/libraries/bitmap_fonts/bitmap_fonts.cmake
@@ -0,0 +1,4 @@
+if(NOT TARGET bitmap_fonts)
+	add_library(bitmap_fonts ${CMAKE_CURRENT_LIST_DIR}/bitmap_fonts.cpp)
+	target_include_directories(bitmap_fonts INTERFACE ${CMAKE_CURRENT_LIST_DIR})
+endif()
---
+++ gfx_pack/libraries/hershey_fonts/hershey_fonts.cmake
@@ -0,0 +1,12 @@
+set(LIB_NAME hershey_fonts)
+
+if(NOT TARGET hershey_fonts)
+	add_library(${LIB_NAME} INTERFACE)
+
+	target_sources(${LIB_NAME} INTERFACE
+	  ${CMAKE_CURRENT_LIST_DIR}/${LIB_NAME}.cpp
+	  ${CMAKE_CURRENT_LIST_DIR}/${LIB_NAME}_data.cpp
+	)
+
+	target_include_directories(${LIB_NAME} INTERFACE ${CMAKE_CURRENT_LIST_DIR})
+endif()
